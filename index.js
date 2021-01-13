0c088d2b976ae8f5fe35b83017b2c61876cfb3e3
Michael Robards <mikerobards@icloud.com>
2021-01-13 09:59:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let robot = document.querySelector('.robot');
+
+//Challenge: Make the robot move when you click it. Don't forget to share you robot on
+//Discord! Feel free to play around with the styling before doing so!
+
+//declare a variable
+let tbmove = 0;
+let lfmove = 0;
+
+function moveRobot(event) {
+    //add code here
+    if (event.defaultPrevented) {
+        return;
+    }
+    switch (event.key) {
+        case 'ArrowUp':
+            tbmove -= 25;
+            robot.style.top = tbmove + 'px';
+            break;
+        case 'ArrowDown':
+            tbmove += 25;
+            robot.style.top = tbmove + 'px';
+            break;
+        case 'ArrowLeft':
+            lfmove -= 25;
+            robot.style.left = lfmove + 'px';
+            break;
+        case 'ArrowRight':
+            lfmove += 25;
+            robot.style.left = lfmove + 'px';
+            break;
+        default:
+            return;
+    }
+}
+console.log(robot);
+
+window.addEventListener('keydown', moveRobot)
